4698367e91e130fd30c9db494dfd2ac622fa0d83
Adithya <jadithya2004@gmail.com>
2025-08-09 21:44:05 +0530

    Update page.js

--- app/about/page.js
+++ app/about/page.js
@@ -6,6 +6,7 @@ import PageViewWrapper from '../components/PageViewWrapper'
 
 export default function AboutPage() {
 
+  
   return (
     <PageViewWrapper>
     <div className="flex flex-col min-h-screen">
@@ -64,7 +65,8 @@ export default function AboutPage() {
               </div>
             </div>
           </div>
-          
+
+            
           <Link href="/" className="inline-flex items-center text-blue-600 hover:text-blue-800 font-semibold transition-colors">
             <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
@@ -73,6 +75,7 @@ export default function AboutPage() {
           </Link>
         </div>
       </div>
+              
       <Footer />
     </div>
     </PageViewWrapper>
